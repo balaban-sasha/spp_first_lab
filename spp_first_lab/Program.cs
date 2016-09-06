64fe6dc6b6aba56308547a08e0f7d8b70829c90e
MSI-PC <MSI-PC@DESKTOP-QESS1M1>
2016-09-06 17:28:06 +0300

    method that to swap elements in array has been created

--- spp_first_lab/Program.cs
+++ spp_first_lab/Program.cs
@@ -15,7 +15,33 @@ namespace spp_first_lab
 
         public class QuickSort
         {
-            public void ToSort(int[] a, int v, int length)
+            IComparer comparer = new ClassForCompare();
+            public void ToSort(int[] array, int left, int right)
+            {
+                int middleElement = array[left + (right - left) / 2];
+                int i = left;
+                int j = right;
+                while(comparer.Compare(i,j)!=(-1))
+                {
+                    while (comparer.Compare(array[i], middleElement) == 1)
+                        i++;
+                    while (comparer.Compare(array[j], middleElement) == (-1))
+                        j--;
+                    if(comparer.Compare(i,j)!=(-1))
+                    {
+                        Swap(array, i, j);
+                        i++;
+                        j++;
+                    }
+                }
+                if (comparer.Compare(i, right) == 1)
+                    ToSort(array, i, right);
+                if (comparer.Compare(left, j) == 1)
+                    ToSort(array, left, j);
+                throw new NotImplementedException();
+            }
+
+            private void Swap(int[] array, int i, int j)
             {
                 throw new NotImplementedException();
             }
